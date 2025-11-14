d8cf5087e52b493db4996bb7d161a58e4f6dd876
Alfred Godley <agodley365@gmail.com>
2025-11-14 06:54:55 +0000

    Added Ruff for python linting/formatting. Configured it to work with Pyright.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -207,15 +207,29 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        -- [[ Go ]]
         -- gopls = {},
-        pyright = {},
+
+        -- [[ Python ]]
+        pyright = {
+          disableOrganizeImports = true,
+          anaylsis = {
+            ignore = { '*' },
+            typeCheckingMode = 'basic',
+          },
+        },
+        ruff = {},
+
+        -- [[ Web dev: Js,Ts,Vue,etc. ]]
         -- See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         ts_ls = {},
+        vue_ls = {},
 
+        -- [[ Lua ]]
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
@@ -230,9 +244,10 @@ return {
             },
           },
         },
-        -- Added
-        vue_ls = {},
+
+        -- [[ Bash ]]
         bashls = {},
+
         -- [[ C++ & Cmake ]]
         clangd = {},
         neocmakelsp = {},
